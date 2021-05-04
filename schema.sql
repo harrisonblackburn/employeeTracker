1351050cae0eec5476dc85b8b96b1c53ea7a4280
harrisonblackburn <blackbhr@yahoo.com>
2021-05-04 17:57:15 -0500

    installed foreign keys in schema.sql

--- schema.sql
+++ schema.sql
@@ -15,5 +15,15 @@ CREATE TABLE role (
   id INT NOT NULL PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,4) NOT NULL,
-  department_id integer NOT NULL
+  department_id INTEGER NOT NULL, 
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL, 
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  manager_id INT NULL
 );
